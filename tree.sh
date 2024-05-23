f50b9c13715520a840c6cec8eebdee225df2a581
Rodrigo França <rodrigo.fla75@gmail.com>
2024-05-22 23:52:45 -0300

    Update tree.sh

--- tree.sh
+++ tree.sh
@@ -2,13 +2,14 @@ git clone https://github.com/LineageOS/android_device_motorola_devon device/moto
 
 git clone https://github.com/LineageOS/android_device_motorola_sm6225-common device/motorola/sm6225-common
 
-git clone https://github.com/LineageOS/android_kernel_motorola_sm6225 kernel/motorola/sm6225
+git clone --depth=1 https://github.com/TheMuppets/proprietary_vendor_motorola_devon vendor/motorola/devon
+
+git clone --depth=1 https://github.com/TheMuppets/proprietary_vendor_motorola_sm6225-common vendor/motorola/sm6225-common
+
+git clone --depth=1 https://github.com/LineageOS/android_kernel_motorola_sm6225 kernel/motorola/sm6225
 
 git clone https://github.com/LineageOS/android_hardware_motorola hardware/motorola
 
 git clone https://github.com/LineageOS/android_system_qcom system/qcom
 
-git clone https://github.com/TheMuppets/proprietary_vendor_motorola_devon vendor/motorola/devon
-
-git clone https://github.com/TheMuppets/proprietary_vendor_motorola_sm6225-common vendor/motorola/sm6225-common
 
