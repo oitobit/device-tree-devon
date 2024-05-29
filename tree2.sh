fd576e315bedff077f331cd9bf174cacc5fc82b8
Rodrigo França <rodrigo.fla75@gmail.com>
2024-05-29 10:45:05 -0300

    SM6225-Project: by josexda
    implementing device tree updates.

---
+++ tree2.sh
@@ -0,0 +1,13 @@
+git clone https://github.com/SM6225-Project/device_motorola_devon device/motorola/devon
+
+git clone https://github.com/SM6225-Project/device_motorola_sm6225-common device/motorola/sm6225-common
+
+git clone https://github.com/SM6225-Project/proprietary_vendor_motorola_devon vendor/motorola/devon
+
+git clone https://github.com/SM6225-Project/proprietary_vendor_motorola_sm6225-common vendor/motorola/sm6225-common
+
+git clone https://github.com/SM6225-Project/kernel_motorola_sm6225 kernel/motorola/sm6225
+
+git clone https://github.com/SM6225-Project/hardware_motorola hardware/motorola
+
+git clone https://github.com/LineageOS/android_system_qcom system/qcom
